c995c81319ec108736f4dacfdfcd76524e96274c
Kristen <kristenwood123@gmail.com>
2021-10-07 18:49:47 -0700

    added hero

---
+++ src/components/Hero.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { HeroSect } from '../styles/Hero.styles'
+
+const Main = () => {
+  return (
+    <HeroSect>
+      <div className="hero__wrapper">
+        <h3 className='hero__hello'>Hello, I'm</h3>
+        <h1 className='hero__name'>Kristen Woodward</h1>
+        <h2 className='hero__front'>Front-End Developer</h2>
+      </div>
+    </HeroSect>
+  )
+}
+
+export default Main
---
+++ src/styles/Hero.styles.js
@@ -0,0 +1,28 @@
+import styled from 'styled-components'
+
+export const HeroSect = styled.section`
+  height: 100vh;
+  background: #272727;
+  color: #fff;
+  font-family: "Montserrat", sans-serif;
+  display: flex;
+  flex-direction: column;
+
+  .hero__wrapper {
+    margin: 2rem;
+  }
+  .hero__hello {
+    color: #DFDFE0;
+    margin-bottom: .5rem;
+  }
+
+  .hero__name {
+    font-weight: 400;
+    font-size: 40px;
+    margin-bottom: 1.5rem;
+  }
+
+  .hero__front {
+
+  }
+`
